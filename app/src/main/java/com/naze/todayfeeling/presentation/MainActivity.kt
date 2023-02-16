8c9a38477ea8fb3ae448d8d5b03f6a62476cfc8c
NaZe0320 <rwt1226@naver.com>
2023-02-16 17:02:56 +0900

    [feat] 바텀 네비게이션 구현

--- app/src/main/java/com/naze/todayfeeling/presentation/MainActivity.kt
+++ app/src/main/java/com/naze/todayfeeling/presentation/MainActivity.kt
@@ -2,6 +2,12 @@ package com.naze.todayfeeling.presentation
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.naze.todayfeeling.R
 import com.naze.todayfeeling.databinding.ActivityMainBinding
 import com.naze.todayfeeling.util.BindingActivity
@@ -9,16 +15,24 @@ import com.naze.todayfeeling.util.BindingActivity
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
 
         initLayout()
+        initNavigation()
     }
 
     private fun initLayout() {
 
     }
+
+    private fun initNavigation() {
+        navController = Navigation.findNavController(this,R.id.main_nav_host_fragment)
+        setupWithNavController(binding.bottomNavigationView,navController)
+    }
+
 
 }
