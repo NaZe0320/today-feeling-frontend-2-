e0d65ac0c872fabc5e79bd08c17c421ca24faabd
NaZe0320 <rwt1226@naver.com>
2023-02-27 04:11:21 +0900

    [feat] writing activity 생성 및 바인딩

---
+++ app/src/main/java/com/naze/todayfeeling/presentation/writing/WritingActivity.kt
@@ -0,0 +1,13 @@
+package com.naze.todayfeeling.presentation.writing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naze.todayfeeling.R
+import com.naze.todayfeeling.databinding.ActivityWritingBinding
+import com.naze.todayfeeling.util.binding.BindingActivity
+
+class WritingActivity : BindingActivity<ActivityWritingBinding>(R.layout.activity_writing) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
